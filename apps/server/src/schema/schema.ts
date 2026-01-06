4c005de6042fc6e71176d28b3b8c254bd2d43ab4
shu12388y <spl.sp999@gmail.com>
2026-01-06 17:48:52 +0530

    added the server graphql

---
+++ apps/server/src/schema/schema.ts
@@ -0,0 +1,34 @@
+// import { GraphQLString,GraphQLSchema,GraphQLObjectType } from "graphql";
+
+import { buildSchema } from "graphql";
+
+// export const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: 'Query',
+//     fields: {
+//       hello: {
+//         type: GraphQLString,
+//         resolve: () => 'Hello world!'
+//       },
+//       start:{
+//         type:GraphQLString,
+//         resolve:()=>'This is how we start'
+//       }
+//     },
+//   }),
+// });
+
+export const schema = buildSchema(
+	`
+    type Query{
+        hello(name:String):String
+
+    }    
+    `,
+);
+
+export const rootValue = {
+	hello: (args:any) => {
+		return "Hello" + args.name;
+	},
+};
---
+++ apps/server/src/server.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { schema,rootValue } from "./schema/schema.js";
+import { ruruHTML } from "ruru/server";
+
+export const app: Express = express();
+
+app.get("/", (_req, res) => {
+	res.type("html");
+	res.end(ruruHTML({ endpoint: "/v1/graphql" }));
+});
+
+app.use(
+	"/v1/graphql",
+	createHandler({
+		schema,
+        rootValue
+	}),
+);
